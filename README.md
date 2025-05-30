15290d3e60a5b79e6fa6beb78104e60394416090
flyotlin <flyotlin@gmail.com>
2025-05-30 23:54:22 +0200

    etl: get data and fit the model

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Chain Anomaly Detector
 
 Detect anomaly on specified chain
+
+訓練一個 AI 模型，偵測是否有異常行為。
+
+## Architecture
--- app/etl/main.py
+++ app/etl/main.py
@@ -1,5 +1,14 @@
+import requests
+import pandas as pd
+
+
+URL = 'https://celo-alfajores.blockscout.com/api/v2/transactions?filter=validated'
+
+
 def main():
-    print("Hello from etl!")
+    res = requests.get(URL).json()
+    df = pd.DataFrame(res['items'])
+    print(df.head())
 
 
 if __name__ == "__main__":
