b0fc666029e850cac53d0041df8483cf509538cc
flyotlin <flyotlin@gmail.com>
2025-05-31 13:43:28 +0200

    feat: Add information icon on the right of history item

--- app/ui/src/components/TransactionItem.vue
+++ app/ui/src/components/TransactionItem.vue
@@ -88,6 +88,21 @@ const truncateHash = (hash: string) => {
           <p class="text-sm text-gray-500">Fee: {{ transaction.fee.value }}</p>
         </div>
       </div>
+      <div class="ml-4 flex-shrink-0">
+        <div class="group relative">
+          <button class="text-gray-400 hover:text-gray-500 focus:outline-none">
+            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
+            </svg>
+          </button>
+          <div class="absolute right-0 bottom-full mb-2 hidden group-hover:block">
+            <div class="bg-gray-900 text-white text-xs rounded py-1 px-2 whitespace-nowrap">
+              Hello placeholder
+            </div>
+            <div class="w-2 h-2 bg-gray-900 transform rotate-45 absolute -bottom-1 right-4"></div>
+          </div>
+        </div>
+      </div>
     </div>
 
     <!-- Collapsible Details Section -->
