c4a43159d73fc44fbe918c0b49210adb4c4f2d51
flyotlin <flyotlin@gmail.com>
2025-05-31 14:53:26 +0200

    style: format small Vue components

--- app/ui/src/components/CollapsibleHeader.vue
+++ app/ui/src/components/CollapsibleHeader.vue
@@ -1,33 +1,37 @@
 <script setup lang="ts">
-defineProps<{
-  title: string
-  isExpanded: boolean
-}>()
+  defineProps<{
+    title: string;
+    isExpanded: boolean;
+  }>();
 
-defineEmits<{
-  (e: 'toggle'): void
-}>()
+  defineEmits<{
+    (e: 'toggle'): void;
+  }>();
 </script>
 
 <template>
-  <div 
+  <div
     class="px-6 py-4 bg-gradient-to-r from-indigo-500 to-indigo-600 flex items-center justify-between cursor-pointer"
     @click="$emit('toggle')"
   >
     <h3 class="text-lg font-medium text-white">{{ title }}</h3>
-    <button 
+    <button
       class="text-white hover:text-indigo-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 rounded-full p-1"
       @click.stop="$emit('toggle')"
     >
-      <svg 
+      <svg
         class="h-5 w-5 transform transition-transform duration-200"
         :class="{ 'rotate-180': !isExpanded }"
-        xmlns="http://www.w3.org/2000/svg" 
-        viewBox="0 0 20 20" 
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
         fill="currentColor"
       >
-        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+        <path
+          fill-rule="evenodd"
+          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+          clip-rule="evenodd"
+        />
       </svg>
     </button>
   </div>
-</template> 
+</template>
